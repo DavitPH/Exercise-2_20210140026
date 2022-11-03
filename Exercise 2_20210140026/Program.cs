0a19645c537d8c25d0981ce2c2ace0ef1698978c
DavitPH <davitph0@gmail.com>
2022-11-03 15:20:15 +0700

    Add project files.

---
+++ Exercise 2_20210140026/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2_20210140026
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
