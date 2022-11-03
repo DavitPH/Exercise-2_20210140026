2bd7beed146450663c80ad5049bbbd3c06468994
DavitPH <davitph0@gmail.com>
2022-11-03 15:36:01 +0700

    deklarasi arr private int ukuran 26

--- Exercise 2_20210140026/Program.cs
+++ Exercise 2_20210140026/Program.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Exercise_2_20210140026
 {
+    //28
     class Program
     {
+        //deklarasi array int ukuran 26
+        private int[] davit = new int[26];
+
+        //deklarasi variabel in untuk menyimpan data pada array yg di input user
+        private int hartono;
+
+        //method menerima masukan
+
 
         static void Main(string[] args)
         {
